fe59039519f632fad94bc4e2d1c95151c1f44502
Pandacowbat <anwespanda@gmail.com>
2019-03-06 23:45:37 +0530

    Added google TTS. Execution time has increased greatly, and I have to look for other faster open source tts systems, perhaps one that can be self hosted.

--- assistant.py
+++ assistant.py
@@ -1,6 +1,12 @@
-import requests,datetime,json,wikipedia
+import requests,datetime,json,wikipedia,os
 from googlesearch import search
+from gtts import gTTS
 
+def talk_back(text):
+    print(text)
+    tts=gTTS(text)
+    tts.save('audio.mp3')
+    os.system('mpg123 -q audio.mp3')
 ## FOR WEATHER REQUESTS
 def weather():
     api_key="bca2453ed5abd8350684b80d66bd95f9"
@@ -21,10 +27,12 @@ def weather():
         current_pressure="0"
         current_humidity="0"
         weather_description="Enter valid city."
-    print("The current temperature is:" +str(current_temp)+" degrees celcius."
+    text=("The current temperature is:" +str(current_temp)+" degrees celcius."
         +"\nThe current pressure is:" +str(current_pressure)+" in hPa units."
         +"\nThe current humidity is:" +str(current_humidity)+"%"
         +"\nDescription:" +weather_description)
+    talk_back(text)
+
 #WEATHER REQUEST ENDS
 
 ## FOR JOKE REQUESTS
@@ -34,13 +42,16 @@ def joke():
     x=response.json()
     joke_setup=x["setup"]
     joke_punchline=x["punchline"]
-    print("You do realize you could just use Alexa instead. But fine, have your joke."
+    text = ("You do realize you could just use Alexa instead. But fine, have your joke."
         +"\n"+joke_setup+"\n..."+joke_punchline)
+    talk_back(text)
 #JOKE REQUESTS ENDS
 
 ##FOR GOOGLE SEARCH
 def google_it_for_me():
-    google_query=input("\nWhat's your query? I'll return some nice links!\n")
+    text="\nWhat's your query? I'll return some nice links!\n"
+    talk_back(text)
+    google_query=input()
     for j in search(google_query, tld="com", lang='en', num=10, start=0, stop=1, pause=1.5):
         print(j)
 #GOOGLE SEARCH ENDS
@@ -48,16 +59,16 @@ def google_it_for_me():
 ##FOR WIKIPEDIA SUMMARY
 def search_wikipedia_for_me():
     wikipedia_query=input("\nWho or what do you want to search for?\n")
-    print (wikipedia.summary(wikipedia_query, sentences=4))
+    talk_back(wikipedia.summary(wikipedia_query, sentences=4))
 #WIKIPEDIA FUNCTION ENDS
 
 # ASK QUERY AND CALL FUNCTIONS
 user = "Panda"
-print("Hey "+user)
+talk_back("Hey " +user)
 while(True):
     query=input("\nHow can I help?\n")
     if "weather" in query:
-        print("Sure, I'll fetch the weather real quick.")
+        talk_back("Sure, I'll fetch the weather real quick.")
         weather()
     elif "time" in query:
         print(datetime.datetime.now())
